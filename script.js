d7be27bf62f3cd70ee6adbfac6d7f1e3aae41fe1
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-06-17 00:18:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -335,11 +335,7 @@ function getTimeOfDay() {
 function random(list) {
     return list[Math.floor(Math.random() * list.length)];
 }
-];
 
-// ===============================
-// 🪨 LUGNSTEN
-// ===============================
 // ===============================
 // 🪨 LUGNSTEN
 // ===============================
